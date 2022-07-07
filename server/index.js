0f08833ef9aa0c88a20cc3076001b3c2d24ea5af
NwaguChima <nwaguchima48@gmail.com>
2022-07-07 02:58:32 +0100

    setup basic route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -23,4 +27,4 @@ mongoose
   )
   .catch((err) => console.log(err.message));
 
-mongoose.set("useFindAndModify", false);
+// mongoose.set("useFindAndModify", false);
