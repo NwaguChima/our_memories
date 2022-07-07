ee7285e7ffc2b2f7cbbaf4eb73bd4c20c012456c
NwaguChima <nwaguchima48@gmail.com>
2022-07-07 03:32:30 +0100

    added form styles

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,16 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories share
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
