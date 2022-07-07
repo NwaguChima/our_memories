d6ce8366ac60ab9107cb7ff4f158725035fcdd5d
NwaguChima <nwaguchima48@gmail.com>
2022-07-07 03:30:06 +0100

    setup global app styles for client

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,11 @@ import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
