72ee9a98245e123a8afe5373070984f6b8ab6997
NwaguChima <nwaguchima48@gmail.com>
2022-07-07 04:51:09 +0100

    fixed cors issues

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://oluwachumzy:Oluwachumzy1@cluster0.kf15b.mongodb.net/?retryWrites=true&w=majority";
