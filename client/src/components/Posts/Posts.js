fb1f83a07e99a18f9093f372ca7cee350bb8483c
NwaguChima <nwaguchima48@gmail.com>
2022-07-08 16:41:38 +0100

    getting and sending post to post component

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 
 import { useSelector } from "react-redux";
@@ -9,12 +10,21 @@ const Posts = () => {
   const posts = useSelector((state) => state.posts);
 
   console.log("posts", posts);
-  return (
-    <>
-      <div>Posts</div>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item xs={12} sm={6} key={post._id}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
