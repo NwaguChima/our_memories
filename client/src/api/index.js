8914efac6350031ee9a56332358f760052f0e0bf
NwaguChima <nwaguchima48@gmail.com>
2022-07-09 23:44:13 +0100

    updated update post action

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,4 +7,4 @@ export const fetchPosts = () => axios.get(url);
 export const createPost = (post) => axios.post(url, post);
 
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, post);
+  axios.patch(`${url}/${id}`, updatedPost);
