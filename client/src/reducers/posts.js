9dab7effe9e8889c4915948df2a0735387348a61
NwaguChima <nwaguchima48@gmail.com>
2022-07-09 23:46:53 +0100

    setup case Update reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,11 @@
+import Post from "../components/Posts/Post/Post";
+
 const reducer = (posts = [], action) => {
   switch (action.type) {
+    case "UPDATE":
+      return posts.map((post) =>
+        Post._id === post._id ? action.payload : post
+      );
     case "FETCH_ALL":
       return action.payload;
     case "CREATE":
