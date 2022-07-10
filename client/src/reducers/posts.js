2f1429293671af5a84276a52d764db2d2f60e622
NwaguChima <nwaguchima48@gmail.com>
2022-07-10 02:37:54 +0100

    added delete to reducer

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,6 +2,8 @@ import Post from "../components/Posts/Post/Post";
 
 const reducer = (posts = [], action) => {
   switch (action.type) {
+    case "DELETE":
+      return posts.filter((post) => post.id !== action.id);
     case "UPDATE":
       return posts.map((post) =>
         Post._id === post._id ? action.payload : post
