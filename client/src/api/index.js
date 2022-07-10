df804ea9179457086ac5618eda19e609acb5461d
NwaguChima <nwaguchima48@gmail.com>
2022-07-10 02:36:12 +0100

    setup action for delete

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,3 +8,5 @@ export const createPost = (post) => axios.post(url, post);
 
 export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
+
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
