c712a7f02a35dc04dbd9d251e4ffe6b97006ae00
NwaguChima <nwaguchima48@gmail.com>
2022-07-10 23:00:21 +0100

    setup api and action for likepost

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -39,3 +39,14 @@ export const deletePost = (id) => async (dispatch) => {
     console.log(error);
   }
 };
+
+export const likePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id, post);
+
+    dispatch({ type: "UPDATE", payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
+``;
