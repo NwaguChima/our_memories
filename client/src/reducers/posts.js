df75a91c3c425528a6d06034090e92d002cb2413
NwaguChima <nwaguchima48@gmail.com>
2022-07-11 22:35:56 +0100

    updated constants for reducers

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,17 @@
 import Post from "../components/Posts/Post/Post";
+import { CREATE, DELETE, FETCH_ALL, UPDATE } from "../constants/actionTypes";
 
 const reducer = (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post.id !== action.id);
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
       return posts.map((post) =>
         Post._id === post._id ? action.payload : post
       );
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
     default:
       return posts;
