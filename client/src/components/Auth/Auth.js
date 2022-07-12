a7c56e2550b09bb2efb6463b37d2dd55373c83b3
NwaguChima <nwaguchima48@gmail.com>
2022-07-12 20:47:18 +0100

    setup auth comp header

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -7,11 +7,23 @@ import {
   Typography,
   Container,
 } from "@material-ui/core";
-
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import useStyles from "./styles";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const classes = useStyles();
+  const isSignup = false;
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
