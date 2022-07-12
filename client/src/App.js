ce7a76423c472f02e1f03045ad4595b395d10b26
NwaguChima <nwaguchima48@gmail.com>
2022-07-12 20:26:45 +0100

    setup the home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { getPosts } from "./actions/posts";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
-import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [dispatch, currentId]);
-
   return (
     <Container maxWidth="lg">
       <Navbar />
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Home />
     </Container>
   );
 };
