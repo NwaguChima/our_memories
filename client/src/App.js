c07a4ed7e7376ae2aea1c79373534c42d789e185
NwaguChima <nwaguchima48@gmail.com>
2022-07-12 20:07:49 +0100

    added nav component

--- client/src/App.js
+++ client/src/App.js
@@ -20,17 +20,6 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Memories share
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
       <Grow in>
         <Container>
           <Grid
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <Typography className={classes.heading} variant="h2" align="center">
+        Memories share
+      </Typography>
+      <img
+        className={classes.image}
+        src={memories}
+        alt="memories"
+        height="60"
+      />
+    </AppBar>
+  );
+};
+
+export default Navbar;
