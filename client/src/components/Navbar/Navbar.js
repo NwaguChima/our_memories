be54820f6a8c67161181a699b86a4c0beec1501c
NwaguChima <nwaguchima48@gmail.com>
2022-07-12 20:17:42 +0100

    rendered user image

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,23 +1,43 @@
 import React from "react";
-import { AppBar, Typography } from "@material-ui/core";
-
+import { AppBar, Avatar, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 
 const Navbar = () => {
   const classes = useStyles();
+  const user = null;
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <Typography className={classes.heading} variant="h2" align="center">
-        Memories share
-      </Typography>
-      <img
-        className={classes.image}
-        src={memories}
-        alt="memories"
-        height="60"
-      />
+      <div className={classes.brandContainer}>
+        <Typography
+          component={Link}
+          to="/"
+          className={classes.heading}
+          variant="h2"
+          align="center"
+        >
+          Memories share
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </div>
+      <Toolbar className={classes.toolbar}>
+        {
+            user ? (
+                <div className={classes.profile}>
+                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                </div>
+            ) : (
+                
+            )
+        }
+      </Toolbar>
     </AppBar>
   );
 };
