3afbc021d7dfcd02cc1e721d51d28cfa3d343951
NwaguChima <nwaguchima48@gmail.com>
2022-07-12 20:20:51 +0100

    completed ui for user auth

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Avatar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
@@ -28,15 +28,37 @@ const Navbar = () => {
         />
       </div>
       <Toolbar className={classes.toolbar}>
-        {
-            user ? (
-                <div className={classes.profile}>
-                    <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                </div>
-            ) : (
-                
-            )
-        }
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography variant="h6" className={classes.userName}>
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={() => {}}
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
