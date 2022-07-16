21d64615a56976c0576d9c9142c9b6f765c4d42d
NwaguChima <nwaguchima48@gmail.com>
2022-07-16 07:43:01 +0100

    signup controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -38,3 +38,46 @@ export const signin = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign(
+      {
+        email: result.email,
+        id: result._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.status(200).json({
+      result,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
