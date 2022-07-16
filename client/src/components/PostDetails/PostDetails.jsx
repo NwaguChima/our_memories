370ac4dac9807b0b9f90243327c9de47f23625cd
NwaguChima <nwaguchima48@gmail.com>
2022-07-16 15:38:34 +0100

    setup post detail headers

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {
+  Paper,
+  Typography,
+  CircularProgress,
+  Divider,
+} from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import moment from "moment";
+import { useParams, useNavigate } from "react-router-dom";
+
+import useStyles from "./sytles";
 
 const PostDetails = () => {
   console.log("PostDetails");
