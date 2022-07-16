9d3ca4e9a0df2b338f4cf87cb0648b0a014da5ee
NwaguChima <nwaguchima48@gmail.com>
2022-07-16 07:36:58 +0100

    setup the signin controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,3 +2,39 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 import { User } from "../models/user";
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json({ error: "User does not exist" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.status(200).json({
+      token,
+      result: existingUser,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
